e7f45f1d3e58cb033e7c267ba22ff3968ee674fd
Santiago Medina <santiago.medina@pragma.com.co>
2020-03-03 17:17:26 -0500

    adding map fragments.

--- app/src/main/java/com/sanmed/maptests/MainActivity.java
+++ app/src/main/java/com/sanmed/maptests/MainActivity.java
@@ -1,14 +1,33 @@
 package com.sanmed.maptests;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ActivityNavigator;
+import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnOpenMap = findViewById(R.id.btn_open_map);
+        btnOpenMap.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_open_map){
+            OpenMap();
+        }
+    }
+
+    private void OpenMap() {
+        ActivityNavigator activityNavigator = new ActivityNavigator(this);
+        activityNavigator.navigate(activityNavigator.createDestination().setIntent(new Intent(this, GoogleMapsActivity.class)), null, null, null);
     }
 }
---
+++ app/src/main/java/com/sanmed/maptests/MapFragment.java
@@ -0,0 +1,54 @@
+package com.sanmed.maptests;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+    private MapViewModel mViewModel;
+    private GoogleMap mMap;
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        getMapAsync(this);
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
